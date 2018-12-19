05977c19987c8df23d8d6bf5f7c8e9391dd23310
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2018-12-18 19:06:21 -0500

    Update

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './navbar.css';
+import Logo from '../../img/library.png';
 
 class Navbar extends Component {
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+  componentDidMount(){
+    window.onscroll = this.handleScroll.bind(this)
+  }
+  handleScroll(){
+    console.log(window.scrollY)
+    if(window.scrollY > 400){
+      this.setState({scrolled: true})
+    } else {
+      this.setState({scrolled: false})
+    }
+  }
   render() {
     return (
-      <div className="navbar" >
-        <h3>Book exchanger</h3>
+      <div className={this.state.scrolled ? "navbar scrolled" : "navbar"} >
+        <h3><img src={Logo} height="32px" />Book exchanger</h3>
         <div></div>
         <div className="navbar-btns" >
           <NavLink to="/signup" className="navbar-signup" >Sign up</NavLink>
--- src/layout/home/Home.js
+++ src/layout/home/Home.js
@@ -3,6 +3,7 @@ import '../home/home.css';
 import SearchIcon from '../../img/search.png';
 import MessageIcon from '../../img/chat.png';
 import DealIcon from '../../img/deal.png';
+import { Fade } from 'react-reveal';
 
 class Home extends Component {
   render() {
@@ -11,28 +12,56 @@ class Home extends Component {
         <div className="home-first-container" >
           <div>
             <h1>What is Book exchanger?</h1>
-            <p>Book exchanger is a cross platform built for people wanting to exchange those old books laying in the shelf with other people in the same esituation</p>
+            <p>Book exchanger is a cross platform built for people wanting to exchange those old books laying in the shelf with other people in the same situation</p>
           </div>
         </div>
         <div className="home-second-container" >
-          <div>
-            <h1>How does it work?</h1>
-            <div className="home-instructions-1" >
-              <div>
-                <img src={SearchIcon} />
-                <p>Search books in our library with more than 100.000 options to choose!</p>
+          <div >
+            <Fade left>
+              <h1>HOW DOES IT WORK?</h1>
+            </Fade>
+            <Fade delay={1000} bottom cascade>
+              <div className="home-instructions-1" >
+                <div>
+                  <img src={SearchIcon} />
+                  <p>Search books in our library with more than 100.000 options to choose!</p>
+                </div>
+                <div>
+                  <img src={MessageIcon} />
+                  <p>Let the owner know you are interested in one of his books. He will be able to see your profile and all your books.</p>
+                </div>
+                <div>
+                  <img src={DealIcon} />
+                  <p>Hopefully now you got a deal with the owner and are ready to send him the book and receive yours!</p>
+                </div>
               </div>
-              <div>
-                <img src={MessageIcon} />
-                <p>Let the owner know you are interested in one of his books. He will be able to see your profile and all your books.</p>
-              </div>
-              <div>
-                <img src={DealIcon} />
-                <p>Hopefully now you got a deal with the owner and are ready to send him the book and receive yours!</p>
-              </div>
-            </div>
+            </Fade>
           </div>
         </div>
+        <div className="home-third-container" >
+            <Fade left>
+              <h1>WHY CHOOSE BOOK EXCHANGER?</h1>
+            </Fade>
+            <Fade delay={1000} bottom cascade>
+              <div className="home-our-qualities" >
+                <div>
+                  <img src={SearchIcon} />
+                  <h1>We are the best.</h1>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </div>
+                <div>
+                  <img src={MessageIcon} />
+                  <h1>We are the best.</h1>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </div>
+                <div>
+                  <img src={DealIcon} />
+                  <h1>We are the best.</h1>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </div>
+              </div>
+            </Fade>
+          </div>
       </React.Fragment>
     )
   }
