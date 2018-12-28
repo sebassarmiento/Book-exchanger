98672a3af9e2ad66f42fb7d27650117b19b5a6da
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2018-12-28 12:31:08 -0500

    Changing navbar app animation and creating publish component

---
+++ src/layout/app/publish/Publish.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+class Publish extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            category: '',
+            place: '',
+            image: '',
+            author: '',
+            description: '',
+            pages: 0
+        }
+    }
+    handleChange(e){
+        this.setState({[e.target.name]: e.target.value})
+    }
+    render() {
+        return (
+            <div className="publish-container" >
+                <h1>Publish a book!</h1>
+                <div className="publish-form" >
+                    <input onChange={(e) => this.handleChange(e)} value={this.state.name} name="name" type="text" />
+                    <input onChange={(e) => this.handleChange(e)} value={this.state.category} name="category" type="text" />
+                    <input onChange={(e) => this.handleChange(e)} value={this.state.place} name="place" type="text" />
+                    <input onChange={(e) => this.handleChange(e)} value={this.state.image} name="image" type="text" />
+                    <input onChange={(e) => this.handleChange(e)} value={this.state.author} name="author" type="text" />
+                    <input onChange={(e) => this.handleChange(e)} value={this.state.description} name="description" type="text" />
+                    <input onChange={(e) => this.handleChange(e)} value={this.state.pages} name="pages" type="text" />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Publish;
