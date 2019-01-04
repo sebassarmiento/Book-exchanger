cae55b476c4c8fe9a453d60491b65755b9949908
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2019-01-04 18:48:26 -0500

    Adding select values to publish component

--- src/layout/app/publish/Publish.js
+++ src/layout/app/publish/Publish.js
@@ -36,7 +36,16 @@ class Publish extends Component {
                 this.setState({ publishTry: false })
                 console.log(res)
                 if (res.message === 'Book was added!') {
-                    this.setState({ bookAdded: true })
+                    this.setState({
+                        bookAdded: true,
+                        name: '',
+                        category: '',
+                        place: '',
+                        image: '',
+                        author: '',
+                        description: '',
+                        pages: 0
+                    })
                 } else {
                     this.setState({ publishFail: true })
                 }
@@ -49,9 +58,9 @@ class Publish extends Component {
     publishButton() {
         if (this.state.name.length > 1
             &&
-            this.state.category.length > 2
+            this.state.category.length > 1
             &&
-            this.state.place.length > 2
+            this.state.place.length > 1
             &&
             this.state.author.length > 3
             &&
@@ -64,13 +73,80 @@ class Publish extends Component {
     }
 
     render() {
+        console.log(this.state)
         return (
             <div className="publish-container" >
                 <div className="publish-form" >
                     <div>
                         <input onChange={(e) => this.handleChange(e)} value={this.state.name} name="name" placeholder="Name" type="text" />
-                        <input onChange={(e) => this.handleChange(e)} value={this.state.category} name="category" placeholder="Category" type="text" />
-                        <input onChange={(e) => this.handleChange(e)} value={this.state.place} name="place" placeholder="Location" type="text" />
+                        <div>
+                        <select name="category" value={this.state.category} onChange={(e) => this.handleChange(e)} >
+                            <option value="" disabled defaultValue>Choose a category</option>
+                            <option value="Finance">Finance</option>
+                            <option value="Technology">Technology</option>
+                            <option value="Science">Science</option>
+                            <option value="Science-fiction">Science fiction</option>
+                            <option value="Romance">Romance</option>
+                            <option value="Statistics">Statistics</option>
+                            <option value="Self-improvement">Self-improvement</option>
+                            <option value="Biology">Biology</option>
+                            <option value="Maths">Maths</option>
+                        </select>
+                        <select name="place" value={this.state.place} onChange={(e) => this.handleChange(e)} >
+                            <option value="" disabled defaultValue>Choose a state</option>
+                            <option value="AL">Alabama</option>
+                            <option value="AK">Alaska</option>
+                            <option value="AZ">Arizona</option>
+                            <option value="AR">Arkansas</option>
+                            <option value="CA">California</option>
+                            <option value="CO">Colorado</option>
+                            <option value="CT">Connecticut</option>
+                            <option value="DE">Delaware</option>
+                            <option value="DC">District Of Columbia</option>
+                            <option value="FL">Florida</option>
+                            <option value="GA">Georgia</option>
+                            <option value="HI">Hawaii</option>
+                            <option value="ID">Idaho</option>
+                            <option value="IL">Illinois</option>
+                            <option value="IN">Indiana</option>
+                            <option value="IA">Iowa</option>
+                            <option value="KS">Kansas</option>
+                            <option value="KY">Kentucky</option>
+                            <option value="LA">Louisiana</option>
+                            <option value="ME">Maine</option>
+                            <option value="MD">Maryland</option>
+                            <option value="MA">Massachusetts</option>
+                            <option value="MI">Michigan</option>
+                            <option value="MN">Minnesota</option>
+                            <option value="MS">Mississippi</option>
+                            <option value="MO">Missouri</option>
+                            <option value="MT">Montana</option>
+                            <option value="NE">Nebraska</option>
+                            <option value="NV">Nevada</option>
+                            <option value="NH">New Hampshire</option>
+                            <option value="NJ">New Jersey</option>
+                            <option value="NM">New Mexico</option>
+                            <option value="NY">New York</option>
+                            <option value="NC">North Carolina</option>
+                            <option value="ND">North Dakota</option>
+                            <option value="OH">Ohio</option>
+                            <option value="OK">Oklahoma</option>
+                            <option value="OR">Oregon</option>
+                            <option value="PA">Pennsylvania</option>
+                            <option value="RI">Rhode Island</option>
+                            <option value="SC">South Carolina</option>
+                            <option value="SD">South Dakota</option>
+                            <option value="TN">Tennessee</option>
+                            <option value="TX">Texas</option>
+                            <option value="UT">Utah</option>
+                            <option value="VT">Vermont</option>
+                            <option value="VA">Virginia</option>
+                            <option value="WA">Washington</option>
+                            <option value="WV">West Virginia</option>
+                            <option value="WI">Wisconsin</option>
+                            <option value="WY">Wyoming</option>
+                        </select>
+                        </div>
                         <input onChange={(e) => this.handleChange(e)} value={this.state.image} name="image" placeholder="Image" type="text" />
                         <input onChange={(e) => this.handleChange(e)} value={this.state.author} name="author" placeholder="Author" type="text" />
                         <textarea onChange={(e) => this.handleChange(e)} value={this.state.description} name="description" placeholder="Description" ></textarea>
