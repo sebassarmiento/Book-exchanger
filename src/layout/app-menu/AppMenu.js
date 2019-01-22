f7d00cd9d2156dd117193d147966d78e7d1de392
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2019-01-22 16:31:43 -0500

    Changed app menu style

--- src/layout/app-menu/AppMenu.js
+++ src/layout/app-menu/AppMenu.js
@@ -14,32 +14,11 @@ class AppMenu extends Component {
     let arrow = <i className="fas fa-caret-right"></i>
     return (
       <div className={`app-menu ${this.props.appMenu.status === 'closing' ? 'app-menu-closed' : null}`} >
-        <h3><i onClick={() => this.setState({ menu: !this.state.menu })} className="fas fa-bars"></i>Menu</h3>
-        <div className={this.state.menu ? "menu-items-open" : "menu-items-closed"} >
+        <h1 className="app-menu-title" >Menu</h1>
+        <div className="menu-items-open" >
           <Link className="menu-item" to="/app/feed" ><i className="fas fa-home"></i><span>Home</span></Link>
           <a href="#" className="menu-item" onClick={() => this.props.publish()} ><i className="fas fa-book"></i><span>Publish book</span></a>
           <Link className="menu-item" to="/app/profile" ><i className="fas fa-user"></i><span>My Profile</span></Link>
-        </div>
-        <h3><i onClick={() => this.setState({categories: !this.state.categories})} className="fas fa-ellipsis-h"></i>Categories</h3>
-        <div className={this.state.categories ? "categories-items-open" : "categories-items-closed"} >
-          <Link className="menu-item" to="/app/books/category/sport" >{arrow}<span>Sport</span></Link>
-          <Link className="menu-item" to="/app/books/category/clothes" >{arrow}<span>Clothes</span></Link>
-          <Link className="menu-item" to="/app/books/category/self-improvement" >{arrow}<span>Self-improvement</span></Link>
-          <Link className="menu-item" to="/app/books/category/money" >{arrow}<span>Money</span></Link>
-          <Link className="menu-item" to="/app/books/category/success" >{arrow}<span>Success</span></Link>
-          <Link className="menu-item" to="/app/books/category/motivation" >{arrow}<span>Motivation</span></Link>
-        </div>
-        <h3><i onClick={() => this.setState({chat: !this.state.chat})} className="fas fa-comment"></i>Chat</h3>
-        <div className={this.state.chat ? "settings-items-open" : "settings-items-closed"} >
-          <Link className="menu-item" to="/app/configuration" >Chat 1</Link>
-          <Link className="menu-item" to="/app/configuration" >Chat 2</Link>
-          <Link className="menu-item" to="/app/configuration" >Chat 3</Link>
-        </div>
-        <h3><i onClick={() => this.setState({settings: !this.state.settings})} className="fas fa-cog"></i>Settings</h3>
-        <div className={this.state.settings ? "settings-items-open" : "settings-items-closed"} >
-          <Link className="menu-item" to="/app/configuration" >Configuration</Link>
-          <Link className="menu-item" to="/app/configuration" >Configuration</Link>
-          <Link className="menu-item" to="/app/configuration" >Configuration</Link>
         </div>
       </div>
     )
